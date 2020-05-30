841091a88db7dc6646e957bcd664d727124aa054
Bharath <bharath98mohan@gmail.com>
2020-05-30 19:42:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       disabled: true
+    }
+  }
+
+  handleChange = (e) => {
+    if(e.target.value.length >= 6) {
+      this.setState({
+        disabled: false
+      })
+    }
+    else {
+      this.setState({
+        disabled: true
+      })
+    }
+  }
+  
+  render() {
+    const {disabled} = this.state
+    return (
+      <div className="App">
+        <form>
+          <input type="text" placeholder="User Id" onChange={this.handleChange} />
+          <br/>
+          <button disabled={disabled}>Proceed</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
